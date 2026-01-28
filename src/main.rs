09eaa4604d195206be52631d832734e3488877ab
VamsiMyla916 <mylavamsikrishnasai@gmail.com>
2026-01-27 20:27:43 -0800

    Implemented Struct concept and additional feature: To search for a keyword

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::io;
-
-fn main(){
+struct Note{
+    title:String,
+    content:String,
+}
+fn main() {
     println!("Enter the password:");
 loop{
 
@@ -24,13 +27,14 @@ else{
 }
 }
 
-    let mut notes:Vec<String> = Vec::new();
+    let mut notes:Vec<Note> = Vec::new();
     
     loop{
         println!("--- NOTE KEEPER ---");
         println!("1. Add Note");
         println!("2. List Notes");
         println!("3. Exit");
+        println!("4. Search for Keyword");
         println!("Enter choice:");
 
         let mut input = String::new();
@@ -38,24 +42,37 @@ else{
         let choice = input.trim();
     
     if choice == "1"{
-        println!("Enter a String:");
+        println!("Enter your Title:");
     
-        let mut new_notes = String::new();
+        let mut title_input = String::new();
     
-        io::stdin().read_line(&mut new_notes).expect("Failed to read");
+        io::stdin().read_line(&mut title_input).expect("Failed to read");
     
-        let new_notes = new_notes.trim().to_string();
+        let title_input = title_input.trim().to_string();
+
+        println!("Enter your Content:");
+        let mut content_input = String::new();
+
+        io::stdin().read_line(&mut content_input).expect("Failed to read");
+
+        let content_input = content_input.trim().to_string();
+
+        let note_object = Note{
+            title:title_input,
+            content: content_input,
+        };
     
-        println!("Notes saved,{new_notes}");
+        println!("Notes saved!");
     
-        notes.push(new_notes);
+        notes.push(note_object);
     }
     else if choice == "2"{
     
         println!("----The following is the list of items----");
-        for new_notes in &notes{
+        for note_object in &notes{
             
-           println!("{new_notes}"); 
+           println!("{}",note_object.title);
+           println!("{}",note_object.content); 
         }
     }
     else if choice == "3"{
@@ -63,6 +80,27 @@ else{
        println!("Exited successfully");
        break;
     }
+    else if choice == "4"{
+
+        println!("Enter the keyword:");
+
+        let mut key = String::new();
+
+        io::stdin().read_line(&mut key).expect("Failed to read");
+
+        let key = key.trim();
+
+        for note_object in &notes{
+
+           if note_object.title.to_lowercase().contains(&key.to_lowercase()){
+
+             println!("{}",note_object.title);
+             println!("{}",note_object.content);
+           }
+
+        }
+    }
+
     else{
         println!("Invalid error! Please enter a number from the choices provided only.")
     }
